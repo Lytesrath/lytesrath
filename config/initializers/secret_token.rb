acec06ea219aacada43a226216a6e49d18c80a44
Robert D. Muns <robert.muns@live.com>
2013-04-03 16:37:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lytesrath::Application.config.secret_token = '8bbe682ac466c44d472f0fc80b5f69a4de6a9bf065fea67fb91c0d96e2cb0577db7dd1439b87be258396c72454f6a832463518e8285dd0e266c64c0d9866ed64'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lytesrath::Application.config.session_store :cookie_store, key: '_lytesrath_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lytesrath::Application.config.session_store :active_record_store
